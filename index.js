157f00df8434f46c826b6220d5f8b0990fbe6c6f
Jeffrey Cohetero <jeffreycoheter0@gmail.com>
2025-10-02 14:36:54 -0400

    Function problem 3

--- index.js
+++ index.js
@@ -9,17 +9,29 @@
 }
 (fiveToOneHundred(100));
 */
+
 // Problem 2
+/*
 const multiplesOfThree = (n) => {
     for (let i = 1; i <= n; i++) {
         let threeMultiples = i;
         if (i % 3 === 0) {
             console.log(threeMultiples)
-        } else {
-
         }
-
-
     }
 }
 multiplesOfThree((100));
+*/
+
+//Probelm 3
+
+const multiplesOfThreeOrFives = (n) => {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            console.log(i)
+        } else {
+
+        }
+    }
+}
+multiplesOfThreeOrFives(100);
