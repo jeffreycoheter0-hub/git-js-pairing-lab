2fc18ce0d0eaa67f0c006ef17549d9a45758b937
Felix Vargas <felix70529@gmail.com>
2025-10-02 13:55:44 -0400

    Completed problem 2

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-//Code your solutions in this file
-const fiveToOneHundred = (n) => {
+//Code your solutions in this file 
+
+// Problem 1 
+/* const fiveToOneHundred = (n) => {
     for (let i = 5; i <= n; i++) {
         let orderNumber = i
         console.log(orderNumber);
     }
 }
 (fiveToOneHundred(100));
+*/
+// Problem 2
+const multiplesOfThree = (n) => {
+    for (let i = 1; i <= n; i++) {
+        let threeMultiples = i;
+        if (i % 3 === 0) {
+            console.log(threeMultiples)
+        } else {
+
+        }
+
+
+    }
+}
+multiplesOfThree((100));
