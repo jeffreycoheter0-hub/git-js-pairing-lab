8988d1fa1836874c18b350bd81f06de8821238ef
Jeffrey Cohetero <jeffreycoheter0@gmail.com>
2025-10-02 11:46:24 -0400

    Completed Problem 1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 //Code your solutions in this file
+const fiveToOneHundred = (n) => {
+    for (let i = 5; i <= n; i++) {
+        let orderNumber = i
+        console.log(orderNumber);
+    }
+}
+(fiveToOneHundred(100));
